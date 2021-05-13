816071eddba03f99d73557930054731ea4f64158
nataehoon <skeoddla@gmail.com>
2021-05-13 10:53:17 +0900

    aa

--- kids/src/kids/board/emplo_board/controller/UpdateEmpBoardController.java
+++
@@ -1,7 +0,0 @@
-package kids.board.emplo_board.controller;
-
-import kids.common.controller.SuperClass;
-
-public class UpdateEmpBoardController extends SuperClass {
- 
-}
--- kids/src/kids/board/notification_board/controller/DeleteNotification_boardController.java
+++
@@ -1,7 +0,0 @@
-package kids.board.notification_board.controller;
-
-import kids.common.controller.SuperClass;
-
-public class DeleteNotification_boardController extends SuperClass {
- 
-}
